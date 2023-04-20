86605af254db72afece215fd691b11199d2b2829
Rishi Sheth <rishi.sheth@ibotta.com>
2023-04-20 12:23:57 -0400

    update imports

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  plugins: [
-    require('postcss-preset-env'),
-    require('postcss-import')
-  ]
-}
+export const plugins = [
+  require('postcss-preset-env'),
+  require('postcss-import')
+];
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const TranslationsPlugin = require('./webpack/translations-plugin')
-const devDependencies = require('./package.json').devDependencies
+import { resolve, join } from 'path'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import MiniCssExtractPlugin, { loader as _loader } from 'mini-css-extract-plugin'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import TranslationsPlugin from './webpack/translations-plugin'
+import { devDependencies } from './package.json'
 
 // this function reads Zendesk Garden npm dependencies from package.json and
 // creates a jsDelivr url
@@ -35,74 +35,71 @@ const externalAssets = {
   ]
 }
 
-module.exports = {
-  entry: {
-    app: [
-      '@babel/polyfill',
-      './src/javascripts/locations/ticket_sidebar.js',
-      './src/index.css'
-    ]
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist/assets')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
-        }
-      },
-      {
-        type: 'javascript/auto',
-        test: /\.json$/,
-        include: path.resolve(__dirname, './src/translations'),
-        use: './webpack/translations-loader'
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: 'css-loader', options: { url: false } },
-          'postcss-loader'
-        ]
-      }
-    ]
-  },
-
-  plugins: [
-    // Empties the dist folder
-    new CleanWebpackPlugin({
-      verbose: true,
-      cleanOnceBeforeBuildPatterns: [path.join(process.cwd(), 'dist/**/*')]
-    }),
-
-    // Copy over static assets
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/manifest.json', to: '../[name][ext]' },
-        { from: 'src/images/*', to: './[name][ext]' }
-      ]
-    }),
-
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    }),
-
-    new TranslationsPlugin({
-      path: path.resolve(__dirname, './src/translations')
-    }),
-
-    new HtmlWebpackPlugin({
-      warning: 'AUTOMATICALLY GENERATED FROM ./src/templates/iframe.html - DO NOT MODIFY THIS FILE DIRECTLY',
-      vendorCss: externalAssets.css.filter(path => !!path),
-      vendorJs: externalAssets.js,
-      template: './src/templates/iframe.html',
-      filename: 'iframe.html'
-    })
+export const entry = {
+  app: [
+    '@babel/polyfill',
+    './src/javascripts/locations/ticket_sidebar.js',
+    './src/index.css'
   ]
 }
+export const output = {
+  filename: '[name].js',
+  path: resolve(__dirname, 'dist/assets')
+}
+export const module = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react']
+      }
+    },
+    {
+      type: 'javascript/auto',
+      test: /\.json$/,
+      include: resolve(__dirname, './src/translations'),
+      use: './webpack/translations-loader'
+    },
+    {
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        _loader,
+        { loader: 'css-loader', options: { url: false } },
+        'postcss-loader'
+      ]
+    }
+  ]
+}
+export const plugins = [
+  // Empties the dist folder
+  new CleanWebpackPlugin({
+    verbose: true,
+    cleanOnceBeforeBuildPatterns: [join(process.cwd(), 'dist/**/*')]
+  }),
+
+  // Copy over static assets
+  new CopyWebpackPlugin({
+    patterns: [
+      { from: 'src/manifest.json', to: '../[name][ext]' },
+      { from: 'src/images/*', to: './[name][ext]' }
+    ]
+  }),
+
+  new MiniCssExtractPlugin({
+    filename: '[name].css'
+  }),
+
+  new TranslationsPlugin({
+    path: resolve(__dirname, './src/translations')
+  }),
+
+  new HtmlWebpackPlugin({
+    warning: 'AUTOMATICALLY GENERATED FROM ./src/templates/iframe.html - DO NOT MODIFY THIS FILE DIRECTLY',
+    vendorCss: externalAssets.css.filter(path => !!path),
+    vendorJs: externalAssets.js,
+    template: './src/templates/iframe.html',
+    filename: 'iframe.html'
+  })
+]
